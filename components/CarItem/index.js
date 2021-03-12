3524420094652c5ea88e3446ef7529909f58050d
Harris Butler <hbutler5@patriots.uttyler.edu>
2021-03-12 16:26:48 -0600

    added button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import {View, Text, ImageBackground} from 'react-native';
 import styles from './styles.js';
-
+import StyledButton from '../StyledButton'; 
 const CarItem = () => {
     return (
         <View style={styles.carContainer}>
@@ -15,7 +15,21 @@ const CarItem = () => {
           <Text style={styles.subtitle}>Starting at $69,000</Text>
         </View>
 
+        <StyledButton 
+          type="primary" 
+          content={"Custom Order"} 
+          onPress={() => {
+            console.warn('Custom Order was pressed'); 
+          }}
+          />
 
+        <StyledButton 
+          type="secondary" 
+          content={"Existing Inventory"} 
+          onPress={() => {
+            console.warn('Existing Inventory was pressed'); 
+          }}
+          />
       </View>
     )
 }
